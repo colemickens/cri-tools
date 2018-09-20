d6be53e546acfb71d720db0413be4db8ca30bf02
Cole Mickens <cole@kix.cluster.lol>
2018-09-20 07:25:18 +0000

    sudo

--- run-redis-crictl.sh
+++ run-redis-crictl.sh
@@ -7,19 +7,20 @@ set -euo pipefail
 # requires patch: https://github.com/kubernetes-sigs/cri-tools/pull/383
 
 RUNTIME="kata"
+##RUNTIME="io.containerd.runtime.kata.v2"
 
-crictl stopp $(crictl pods -q)
-crictl rmp $(crictl pods -q)
+sudo crictl stopp $(crictl pods -q)
+sudo crictl rmp $(crictl pods -q)
 
 image="docker.io/library/redis:alpine"
 
 cat<<EOF >"/tmp/pod-config.json"
 {
   "metadata": {
-    "name": "redis0",
+    "name": "rediscrictl0",
     "namespace": "default",
     "attempt": 1,
-    "uid": "redis0"
+    "uid": "rediscrictl0"
   },
   "logDirectory": "/tmp",
   "linux": {}
@@ -28,7 +29,7 @@ EOF
 cat<<EOF >"/tmp/container-config.json"
 {
   "metadata": {
-    "name": "redis0",
+    "name": "rediscrictl0",
     "attempt": 2
   },
   "image":{
@@ -43,14 +44,14 @@ EOF
 # TODO: at this layer does it default to running the iamges command, or is the higher level expected to parse and do it?
  # - yes, it will default to the image command, as we see here ( iremoed the command frm the linked guide, just to see)
 
-crictl pull "${image}"
+sudo crictl pull "${image}"
 
 podid="$(crictl --debug runp --runtime="${RUNTIME}" "/tmp/pod-config.json")"
 
 # TODO: why is pod-config repeated, and passed by id???
 containerid="$(crictl --debug create ${podid} /tmp/container-config.json /tmp/pod-config.json)"
 
-crictl start "${containerid}"
+sudo crictl start "${containerid}"
 sleep 1
-crictl exec -i -t "${containerid}" top
+sudo crictl exec -i -t "${containerid}" top
 
--- run-redis-ctr.sh
+++ run-redis-ctr.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+set -x
+
 RUNTIME="io.containerd.runtime.kata.v2" # fails with: ctr: no such file or directory: not found
 #RUNTIME="io.containerd.runtime.v1.linux" # works
 
 image="docker.io/library/redis:alpine"
 
-ctr container delete redis0
-ctr images pull "${image}"
-trace ctr run \
+sudo ctr container delete redis0
+sudo ctr images pull "${image}"
+sudo ctr run \
   --runtime "${RUNTIME}" \
   "${image}" redis0 /bin/ash
 
