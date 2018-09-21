7393d27654c05ebad8ee2409e061e9e700426e89
Cole Mickens <cole.mickens@gmail.com>
2018-09-21 10:07:06 +0000

    ugh, deja vu

--- run-redis-ctr.sh
+++ run-redis-ctr.sh
@@ -3,17 +3,22 @@
 set -x
 set -euo pipefail
 
-RUNTIME="${RUNTIME:-"io.containerd.kata.v2"}"
-#RUNTIME="io.containerd.runc.v1"
+RUNTIME="io.containerd.kata.v2"
+RUNTIME="io.containerd.runc.v1"
 
+NAME="redis-ctr"
 image="docker.io/library/redis:alpine"
 
-sudo ctr --debug task kill -s 9 redisctr0 || true # TODO: why is -s9 needed? crictl isn't so aggressive?
-sudo ctr --debug task delete redisctr0 || true
-sudo ctr --debug container delete redisctr0 || true
+function cleanup() {
+  sudo crictl stopp $(crictl pods -q)
+  sudo crictl rmp $(crictl pods -q)
+}
+trap cleanup EXIT
+cleanup
+
+sudo ctr --debug task kill -s 9 ${NAME} || true # TODO: why is -s9 needed? crictl isn't so aggressive?
+sudo ctr --debug task delete ${NAME} || true
+sudo ctr --debug container delete ${NAME} || true
 sudo ctr --debug images pull "${image}"
-sudo ctr --debug run \
-  --tty \
-  --runtime "${RUNTIME}" \
-  "${image}" redisctr0 /bin/ash
+sudo ctr --debug run --tty --runtime "${RUNTIME}" "${image}" ${NAME} /bin/ash
 
